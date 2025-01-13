6b07f1787285e76597a1113beb8b34f88836424c
PyxelCodes <pyxeldev@icloud.com>
2025-01-13 23:37:06 +0100

    add different error message to website

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -29,58 +29,48 @@ async function getGlobalStats(): Promise<GlobalStatsType> {
 export default async function Home() {
     try {
         const user = await currentUser();
-        const [data, globalStats] = await Promise.all([
-            getData(user ? user.externalAccounts[0].externalId : null),
-            getGlobalStats(),
-        ]);
 
-        // if (!user) {
-        //     return (
-        //         <main className="min-h-screen p-8 bg-gradient-to-br from-[#9E72C3] to-[#0F0529] animate-gradient-x">
-        //             <div className="max-w-7xl mx-auto">
-        //                 <div className="bg-white p-8 rounded-lg shadow-xl mb-8">
-        //                     <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
-        //                         Welcome to Activity Tracker
-        //                     </h1>
-        //                     <p className="mb-6 text-center text-gray-600">
-        //                         Please sign in to view your personal dashboard
-        //                     </p>
-        //                     <SignInButton mode="modal">
-        //                         <div className="flex justify-center items-center">
-        //                             <Button
-        //                                 size={'lg'}
-        //                                 className="bg-[#924DBF] p-7 text-lg"
-        //                             >
-        //                                 Sign In
-        //                             </Button>
-        //                         </div>
-        //                     </SignInButton>
-        //                 </div>
-        //                 <GlobalStats activities={globalStats.activities} />
-        //             </div>
-        //         </main>
-        //     );
-        // }
+        try {
+            const [data, globalStats] = await Promise.all([
+                getData(user ? user.externalAccounts[0].externalId : null),
+                getGlobalStats(),
+            ]);
 
-        return (
-            <main className="min-h-screen p-8 bg-gradient-to-br from-[#7338A0] to-[#0F0529] animate-gradient-x">
-                <div className="max-w-7xl mx-auto">
-                    <UserInfoCard
-                        user={user}
-                        apiUser={data ? data.user : null}
-                    />
-                    <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
-                        <ActivityList
-                            activities={data ? data.activities : null}
+            return (
+                <main className="min-h-screen p-8 bg-gradient-to-br from-[#7338A0] to-[#0F0529] animate-gradient-x">
+                    <div className="max-w-7xl mx-auto">
+                        <UserInfoCard
+                            user={user}
+                            apiUser={data ? data.user : null}
                         />
-                        <ActivityCharts
-                            activities={data ? data.activities : null}
-                        />
+                        <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
+                            <ActivityList
+                                activities={data ? data.activities : null}
+                            />
+                            <ActivityCharts
+                                activities={data ? data.activities : null}
+                            />
+                        </div>
+                        <GlobalStats activities={globalStats.activities} />
                     </div>
-                    <GlobalStats activities={globalStats.activities} />
-                </div>
-            </main>
-        );
+                </main>
+            );
+        } catch (error) {
+            console.error('Error fetching data:', error);
+
+            return (
+                <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-gradient-to-br from-[#7338A0] to-[#0F0529] animate-gradient-x">
+                    <div className="bg-white p-8 rounded-lg shadow-xl">
+                        <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
+                            Oops! Something went wrong
+                        </h1>
+                        <p className="mb-6 text-center text-gray-600">
+                            Failed to fetch remote data
+                        </p>
+                    </div>
+                </main>
+            );
+        }
     } catch (error) {
         console.error('Error fetching user:', error);
         return (
