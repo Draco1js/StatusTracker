e051365b35b9669a3fc25cdc763b249950d719b2
PyxelCodes <pyxeldev@icloud.com>
2025-01-14 01:57:47 +0100

    parse ' out

--- bot/src/views/activity.ts
+++ bot/src/views/activity.ts
@@ -2,7 +2,7 @@ import convertMs from "../utils/convertMs";
 import emojis from '../emojis.json';
 
 export default function activityView(activity: Activity) {
-    let emojiName = activity.name.normalize('NFC').replace(/ /gm, "").replace(/\-/gm, "")
+    let emojiName = activity.name.normalize('NFC').replace(/ /gm, "").replace(/\-/gm, "").replace(/'/gm, "");
     if (emojiName == "TomClancy'sRainbowSixSiege") emojiName = "RainbowSixSiege";
     let emojiId: string = (emojis as Emojis)[emojiName];
     let emoji = emojiId ? `<:${emojiName}:${emojiId}> ` : '';
